c83dcdd30a52f1b252eacb7c28825dad7e29f382
AleksandraSimeonova <simeonova.aleksandra9@gmail.com>
2025-08-02 23:23:23 +0300

    set html and css for recipe parent component

--- share_your_recepie/src/app/features/posts/post-board/post-board.ts
+++ share_your_recepie/src/app/features/posts/post-board/post-board.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
+import { PostItem } from '../post-item/post-item';
+import { CommonModule } from '@angular/common';
+import { Recipe } from '../../../models';
+import { Observable } from 'rxjs';
+import { RecipeService } from '../../../core/services/posts.service';
 
 @Component({
   selector: 'app-post-board',
-  imports: [RouterModule],
+  imports: [CommonModule, RouterLink, PostItem, RouterModule],
   templateUrl: './post-board.html',
   styleUrl: './post-board.css'
 })
 export class PostBoard {
+
+  recepieId: string = '688de8bfdb859a6eae80ca94';
+  recepies$: Observable<Recipe[]>;
+  
+  constructor(
+
+    private recepieService: RecipeService) {
+  
+    this.recepies$ = this.recepieService.getAll();
+   
+  }
+
+  trackById(index: number, recipe: Recipe): string {
+  return recipe._id;
+}
+
 
 }
