ccfdae4c5d704e034adf489043fc8dfcf064b2de
AleksandraSimeonova <simeonova.aleksandra9@gmail.com>
2025-08-18 17:14:37 +0300

    header component

---
+++ share_your_recepie/src/app/features/shared/components/header/header.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../../core/services';
+
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+ protected authService = inject(AuthService);
+  private router = inject(Router);
+
+  readonly isLoggedIn = this.authService.isLoggedIn;
+  readonly currentUser = this.authService.currentUser;
+
+
+  logout(): void {
+  this.authService.logout().subscribe({
+    next: () => this.router.navigate(['/home']),
+    error: (err) => console.error('Logout error', err)
+  });
+}
+   ///  logout(): void {
+   ///    console.log('Logout activated')
+   ///    this.authService.logout();
+   ///    this.router.navigate(['/home']);
+   ///  }
+
+}
