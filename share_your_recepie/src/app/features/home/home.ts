2fccb0930befd3c65f27b4811d5c43f2a2de32da
AleksandraSimeonova <simeonova.aleksandra9@gmail.com>
2025-08-18 17:49:04 +0300

    navigation according login user

--- share_your_recepie/src/app/features/home/home.ts
+++ share_your_recepie/src/app/features/home/home.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../core/services';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
 
+  protected authService = inject(AuthService);
+  readonly isLoggedIn = this.authService.isLoggedIn
+  readonly currentUser = this.authService.currentUser
 }
