d28cb48863616d141bc24dacf5888801e0e50f10
AleksandraSimeonova <simeonova.aleksandra9@gmail.com>
2025-08-18 12:16:16 +0300

    update func for profile in user controller

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import userService from "../service/userService.js";
+import { isAuth } from "../middlewares/authMiddleware.js";
 
 const userController = Router();
 
@@ -11,8 +12,11 @@ userController.post('/register', async (req, res) => {
 
     res.json({
         _id: user.id,
+        name: user.name,
         accessToken: token,
         email: user.email,
+        postedRecipes: user.postedRecipes || [], 
+        likedRecipes: user.likedRecipes || [],  
     });
 });
 
@@ -28,12 +32,39 @@ userController.post('/login', async (req, res) => {
     });
 });
 
-userController.get('/logout', async (req, res) => {
+userController.get('/logout', isAuth, async (req, res) => {
     const token = req.headers['x-authorization'];
 
     await userService.invalidateToken(token);
 
     res.json({});
+});
+
+userController.put('/:id', isAuth, async (req, res) => {
+    try {
+
+        const userId = req.params.id;
+
+        // Ако userId не съвпада с този в токена (req.user._id)
+        if (req.user._id !== userId) {
+            return res.status(403).json({ message: 'Not authorized to update this user' });
+        }
+
+        const updateData = req.body;
+
+        const updatedUser = await userService.update(userId, updateData);
+
+        res.json({
+            _id: updatedUser.id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            postedRecipes: updatedUser.postedRecipes || [],
+            likedRecipes: updatedUser.likedRecipes || [],
+            
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
 export default userController;
