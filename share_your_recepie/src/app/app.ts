702aea67afdaf985be836b1d4d4da881d4185d60
AleksandraSimeonova <simeonova.aleksandra9@gmail.com>
2025-08-18 17:13:29 +0300

    error interceptor and service

--- share_your_recepie/src/app/app.ts
+++ share_your_recepie/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header, Footer } from "./features/shared";
+import { Header, Footer } from "./features/shared/components";
 import { HttpClient } from '@angular/common/http';
+import { ErrorNotification } from "./features/shared/components/error-notification/error-notification";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, ErrorNotification],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
